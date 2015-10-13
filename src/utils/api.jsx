fe6a3a9664f62fa4f89fd4aa5530411663b0f57b
David Krett <david@what2do.asia>
2015-10-12 20:46:48 -0400

    imgur client'

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = 'd2cf3557550bc1f';
+
+module.exports = window.api = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID' + apiKey
+      }
+    })
+    .then(function(response){
+      console.log(response);
+    })
+  }
+};
