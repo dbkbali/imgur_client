c4f2101a8f998b4b94827d06c62136b93973bc3d
David Krett <david@what2do.asia>
2015-10-12 23:50:28 -0400

    reflux implementation

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,15 +2,15 @@ var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = 'd2cf3557550bc1f';
 
-module.exports = window.api = {
+module.exports = {
   get: function(url) {
     return fetch(rootUrl + url, {
       headers: {
-        'Authorization': 'Client-ID' + apiKey
+        'Authorization': 'Client-ID ' + apiKey
       }
     })
     .then(function(response){
-      console.log(response);
+      return response.json()
     })
   }
 };
