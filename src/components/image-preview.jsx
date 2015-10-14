0e3a2f00df5340298bb94ccf98c62f54118e5350
dbkbali <david@what2do.asia>
2015-10-13 21:26:48 -0400

    video handling

--- src/components/image-preview.jsx
+++ src/components/image-preview.jsx
@@ -6,9 +6,18 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      hovering: false
+    }
+  },
   render: function() {
-    return <div className="image-preview">
-      {this.image()}
+    return <div
+      className="image-preview"
+      onMouseEnter={this.handleMouseEnter}
+      onMouseLeave={this.handleMouseLeave}
+      >
+      {this.props.animated && this.state.hovering ? this.video() : this.image()}
     </div>
   },
   image: function() {
@@ -16,5 +25,16 @@ module.exports = React.createClass({
 
     return <img src={link} />
 
+  },
+  video: function() {
+    <video preload='auto' autoPlay='autoPlay' loop='loop' webkit-playsinline>
+      <source src={this.props.mp4} type='video/mp4' ></source>
+    </video>
+  },
+  handleMouseEnter: function() {
+    this.setState({hovering: true});
+  },
+  handleMouseLeave: function() {
+    this.setState({hovering: false});
   }
 });
